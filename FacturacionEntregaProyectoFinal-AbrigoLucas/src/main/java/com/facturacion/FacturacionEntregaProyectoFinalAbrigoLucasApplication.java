356b78dbed3ddb24c5404d0eed47a37b0e0e32e4
Lucas Abrigo <luca.mdq@gmail.com>
2024-05-06 23:53:05 -0300

    First commit

---
+++ FacturacionEntregaProyectoFinal-AbrigoLucas/src/main/java/com/facturacion/FacturacionEntregaProyectoFinalAbrigoLucasApplication.java
@@ -0,0 +1,13 @@
+package com.facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionEntregaProyectoFinalAbrigoLucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturacionEntregaProyectoFinalAbrigoLucasApplication.class, args);
+	}
+
+}
